2c5f5667e8dd3bce72940aeadc23d2b200f19bdf
marina <renakhamis@gmail.com>
2023-04-21 14:44:19 +0200

    .

--- src/store.js
+++ src/store.js
@@ -12,6 +12,8 @@ const store = createStore(
     initialState,
     compose(
         applyMiddleware(...middleWare),
+
+
         // Implementing Redux Devtools (browser extension)
         // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
